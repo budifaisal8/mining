4737f1fa3c13f7adafcb17448e2f29f6bc0d12f3
budifaisal8 <85603702+budifaisal8@users.noreply.github.com>
2021-06-09 12:34:02 +0700

    Create mibcoin.sh

---
+++ mibcoin.sh
@@ -0,0 +1,4 @@
+# mibcoin 
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a power2b  -o stratum+tcps://stratum-asia.rplant.xyz:17022 -u MtutYsjrhW83Wwexdv9WNGmepnhHBgiHJf.vps1 -t 4

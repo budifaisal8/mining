fcd4fd4eb7de7b43cc1df1fb2d657932cd5a3924
budifaisal8 <85603702+budifaisal8@users.noreply.github.com>
2021-06-09 12:37:26 +0700

    Update mibcoin.sh

--- mibcoin.sh
+++ mibcoin.sh
@@ -1,4 +1,5 @@
 # mibcoin 
+sudo apt update
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 ./cpuminer-sse2 -a power2b  -o stratum+tcps://stratum-asia.rplant.xyz:17022 -u MtutYsjrhW83Wwexdv9WNGmepnhHBgiHJf.vps1 -t 4
